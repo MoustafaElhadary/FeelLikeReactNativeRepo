c8ee78e8a267bd38eda548abebc1542725c76c01
MoustafaElhadary <MoustafaElhadary96@gmail.com>
2018-03-18 15:36:06 -0400

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB0o6MwoIRr1lro3Zd2pB1CrrkyplmJHrA",
-  authDomain: "feellike-17f09.firebaseapp.com",
-  databaseURL: "https://feellike-17f09.firebaseio.com",
-  projectId: "feellike-17f09",
-  storageBucket: "feellike-17f09.appspot.com",
-  messagingSenderId: "772413494327"
+  
 };
 
 module.exports = firebaseConfig
